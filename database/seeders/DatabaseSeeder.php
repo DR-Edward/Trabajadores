579e8e7ece5587d7193098d80015fe64e95e138a
DR-Edward <reyesiovanny@gmail.com>
2020-10-29 05:20:55 -0600

    rename workers seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(DevUsersTableSeeder::class);
-        $this->call(DevWorkerSeeder::class);
+        $this->call(DevWorkersSeeder::class);
     }
 }
